d2ff0ead152b64ee1334defd010139e5e9a5b5a9
CorentinTrt <corentintruffaut@gmail.com>
2022-03-27 23:28:32 -0400

    index.styled.ts - remove border from slick-slide

--- styles/index.styled.ts
+++ styles/index.styled.ts
@@ -3,6 +3,10 @@ import styled  from 'styled-components';
 export const Container = styled.div`
   margin: 0;
   padding: 0;
+  
+  .slick-slide {
+    border: none !important;
+  }
 `;
 
 export const MainHeadingContainer = styled.div`
