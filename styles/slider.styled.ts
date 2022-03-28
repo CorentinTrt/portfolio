9f86431fba58b0cb903492490a9ce76eb636d5d4
CorentinTrt <corentintruffaut@gmail.com>
2022-03-27 23:07:28 -0400

    slider.styled.ts - remove automatic border on slides

--- styles/slider.styled.ts
+++ styles/slider.styled.ts
@@ -4,5 +4,6 @@ export const SliderItem = styled.div`
   display: flex !important;
   min-width: 100vw;
   min-height: 100vh;
+  border: none !important;
   background: ${props => props.theme.black};
 `;
