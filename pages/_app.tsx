f0ea25da42e90eafaa821df3f569b7f10e094d23
CorentinTrt <corentintruffaut@gmail.com>
2022-03-29 15:45:19 -0400

    _app.tsx - add some meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <GlobalStyles />
       <Head>
         <title>{`Corentin's portfolio`}</title>
+        <meta name="description" content="This is a preview of my work" />
+        <meta
+          name="keywords"
+          content="js, javascript, react, react.js, native, vue, vue.js, node, node.js, corentin, truffaut, portfolio, developer, web, montreal, paris"
+        />
+        <meta name="author" content="Corentin Truffaut" />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
