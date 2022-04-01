1d313211c1b54d0af3223d3850853ceee71a45c2
CorentinTrt <corentintruffaut@gmail.com>
2022-04-01 06:50:25 -0400

    next.config.js - enable sass in the project

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
   },
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
