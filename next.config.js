72e9eafb5562c2bd01064fe3fae8408c10b37df4
CorentinTrt <corentintruffaut@gmail.com>
2022-11-21 10:10:55 -0500

    next.config.js - add custom import rules

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-const path = require('path');
-
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    additionalData: `@use "styles/utils/animations.scss" as A; @use "styles/utils/variables.scss" as V;`,
+    excludePaths: "/styles"
   },
 };
 
