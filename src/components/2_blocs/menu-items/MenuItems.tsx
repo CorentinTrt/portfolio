3485f50a35253c0151ca205f85b6d1604c0c1282
CorentinTrt <corentintruffaut@gmail.com>
2022-11-22 17:23:39 -0500

    MenuItems - create component to divide menu into smaller pieces

---
+++ src/components/2_blocs/menu-items/MenuItems.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import s from './style.module.scss';
+
+import emitter from '@src/emitter';
+
+type Props = {
+  isOpen: Boolean;
+  items: [];
+};
+
+const MenuItems = (props: Props) => {
+  const { isOpen, items } = props;
+
+  const handleHoverItem = (i: Number) => {
+    emitter.emit('menu-item::hover', i);
+  };
+
+  const classes = `${s['menu-items']} ${isOpen && s['opened']}`;
+
+  return (
+    <div className={classes}>
+      <div className={s['menu-items--group']}>
+        {items.map((e, i) => (
+          <div id="menu-item" className={s['item']} key={`${e.label}_${i}`}>
+            <div id="number" className={s['number']}>
+              <span>{`0${i}`}</span>
+            </div>
+            <a onMouseEnter={() => handleHoverItem(i)} href="#">
+              {e.label}
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MenuItems;
